c72e33eedb8437700121c286f4733881b2bc1471
morrigan-mm <morrigan.87.mm@gmail.com>
2020-10-08 21:45:42 +0300

    Configure routing

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,39 @@
 import React from "react";
 import PropTypes from "prop-types";
-import MainPage from "../main-page/main-page";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
+import Main from "../main/main";
+import SignIn from "../sign-in/sign-in";
+import AddReview from "../add-review/add-review";
+import Player from "../player/player";
+import Film from "../film/film";
+import MyList from "../my-list/my-list";
 
-const App = ({title, genre, releaseDate}) => (
-  <MainPage title={title} genre={genre} releaseDate={releaseDate}/>
-);
+const App = ({title, genre, releaseDate}) => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main title={title} genre={genre} releaseDate={releaseDate} />
+        </Route>
+        <Route exact path="/login">
+          <SignIn />
+        </Route>
+        <Route exact path="/mylist">
+          <MyList />
+        </Route>
+        <Route exact path="/films/:id">
+          <Film />
+        </Route>
+        <Route exact path="/films/:id/review">
+          <AddReview />
+        </Route>
+        <Route exact path="/player/:id">
+          <Player />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 App.propTypes = {
   title: PropTypes.string.isRequired,
