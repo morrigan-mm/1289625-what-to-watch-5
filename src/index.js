148a7038929f737012d247c1d656d0b3ef93b404
morrigan-mm <morrigan.87.mm@gmail.com>
2020-10-19 18:42:49 +0300

    Fix linter error

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app";
 import {films, promo} from "./mocks/films";
-console.log(films.map((film) => film.genre));
+
 ReactDOM.render(
     <App
       films={films}
